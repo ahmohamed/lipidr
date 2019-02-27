e26f3b6a1c753336e37ea7725df45f31dfeec6d8
Ahmed Mohamed <mohamed@kuicr.kyoto-u.ac.jp>
2019-02-27 16:47:48 +1000

    Differential analysis with design matrix as input

--- R/analysis.R
+++ R/analysis.R
@@ -1,6 +1,7 @@
 #' Differential analysis of lipids between sample groups
 #'
-#' @param ... expressions, or character strings which can be parsed to expressions, specifying contrasts
+#' @param ... expressions, or character strings which can be parsed to expressions, specifying contrasts. 
+#' These are passed to \code{limma::makeContrasts}.
 #' @param data Skyline data.frame created by \code{\link{read.skyline}}, should be normalized and log2 transformed
 #' @param measure name of the column indicating sample names
 #' @param group_col name of the column indicating sample groups
@@ -24,20 +25,50 @@ de_analysis = function(..., data, measure="Area", group_col=NULL){
   symbols = as.character(.quos_syms(quos(...)))
   
   group = colData(data)[[group_col]]
+  if (!all(symbols %in% as.character(group))) {
+    stop("Some of the constrasts variables are not present in group_col")
+  }
   data = data[, group %in% symbols]
   
   group = fct_drop(colData(data)[[group_col]])
   design = model.matrix(~ 0 + group)
   colnames(design) <- gsub("group", "", colnames(design))
-  contr.matrix = limma::makeContrasts(..., levels=colnames(design))
-  
+  return(de_design(..., design=design, data=data, measure=measure))
+}
+
+#' Differential analysis of lipids between sample groups
+#' 
+#' @param ... expressions, or character strings which can be parsed to expressions, specifying contrasts.
+#' These are passed to \code{limma::makeContrasts}. Ignored if \code{coef} is provided.
+#' @param coef column number or column name specifying which coefficient of the linear model is of interest.
+#' @param data Skyline data.frame created by \code{\link{read_skyline}}, should be normalized and log2 transformed
+#' @param measure name of the column indicating sample names
+#' 
+#' @importFrom rlang is_formula
+#' @export
+de_design <- function(..., coef=NULL, design, data, measure="Area") {
+  if (is_formula(design)) {
+    design = model.matrix(design, data=colData(data))
+  } else if (! is.matrix(design)) {
+    stop("design should be a matrix or formula")
+  }
   vfit <- limma::lmFit(assay(data, measure), design)
-  vfit <- limma::contrasts.fit(vfit, contrasts=contr.matrix)
-  efit <- limma::eBayes(vfit)
   
+  if (is.null(coef)) {
+    contr.matrix = limma::makeContrasts(..., levels=colnames(design))
+    vfit <- limma::contrasts.fit(vfit, contrasts=contr.matrix)
+    coef = setNames(c(1:ncol(contr.matrix)), colnames(contr.matrix))
+  } else {
+    if (! coef %in% colnames(design)) {
+      stop("One or more coefficients is not in the design matrix. Allowed values are ", colnames(design))
+    }
+    names(coef) = coef
+  }
+  
+  efit <- limma::eBayes(vfit)
   dimname_x = data@attrs$dimnames[[1]]
-  contrast.list = setNames(c(1:ncol(contr.matrix)), colnames(contr.matrix))
-  top = lapply(contrast.list, function(x) 
+  
+  top = lapply(coef, function(x) 
     limma::topTable(efit, number = Inf, coef = x) %>% tibble::rownames_to_column(dimname_x)) %>%
     bind_rows(.id="contrast")
   
