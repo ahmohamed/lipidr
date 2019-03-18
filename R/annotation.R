aab73c8d861b3d80c81f73d6d1a19a573d747700
Ahmed Mohamed <mohamed@kuicr.kyoto-u.ac.jp>
2019-03-18 13:40:34 +1000

    initial transition to lipidMaps naming convention

--- R/annotation.R
+++ R/annotation.R
@@ -32,31 +32,60 @@ annotate_lipids <- function(data){
 
 #############################################################################################################
 
+.en <- function(...) paste0("(", ..., ")")
+csep = "[/-_]"
 
 itsd_list = c(
   '15:0-18:1(d7) PG','15:0-18:1(d7) PI','Sa1P 17:0','So1P 17:1','15:0-18:1(d7) PE',
   '18:1-d9 SM','Cer d18:1/C12:0','PS 33:1 d7','15:0-18:1(d7) PC','18:1(d7) Lyso PC',
   '18:1(d7) Lyso PE','Cer1P d18:1/12:0','GlucCer d18:1/12:0','LacCer d18:1/12:0','Sa 17:0',
   'So 17:1')
-class_p = "([[:alnum:]]{2,15})"
-chain_p = "(\\d{1,2}:\\d{1,2})"
-isotope_p = "(\\((d\\d)|(IS)\\))"
-notes_p = "(\\(.+\\))"
-mol_p = paste0(class_p, "[ -]", chain_p, "([/-_]", chain_p, ")?\\s*", "([/-_]", chain_p, ")?\\s*", notes_p, "?")
+class_p = "[[:alnum:],-]{2,35}"
+dbond_config = "\\((?:\\d{1,2}[ZE][,]*)+\\)"
+chain_notes = "\\((?:\\d{1,2}[^)]*)+\\)"
+chain_p = paste0("[dth]?\\d{1,2}:\\d{1,2}",.en("?:", chain_notes), "*")
+chain_multi_p = paste0(.en("?:", chain_p, csep, "*"), "+")
+isotope_p = "\\(IS\\)|\\(d\\d\\)"
+notes_p = "\\(.+\\)\\W*"
+notes_multi = paste0(.en("?:", notes_p), "+")
+mol_p = paste0("^", class_p, "[ -]*\\(", chain_multi_p, "\\)", .en("?:", notes_multi), "*")
+mol_multi = paste0(mol_p, "\\s*/\\s*", mol_p)
 
 # PE 32:2, PE 16:0/16:2, GlucCer 18:0/18:0
 p = paste0("^", mol_p, "(\\s*/\\s*",mol_p,")?$") #"^([[:alnum:]]{2,7}) (\\d{2}:\\d{1,2})(/\\d{2}:\\d{1,2})?$"
 itsd = paste0("^", mol_p, ".*", isotope_p)#"^([[:alnum:]]{2,7}) (\\d{2}:\\d{1,2})(/\\d{2}:\\d{1,2})?(\\(d\\d\\))$"
 
+.preprocess_molecule_names <- function(lipids_list) {
+  olipids = trimws(lipids_list)
+  
+  # PC(O-32:0) --> PCO-32:0
+  # PC(O-16:0_18:1)/PC(P-16:2_18:1) --> PCO-16:0_18:1/PCP-16:2_18:1
+  # PC(O-16:0/18:2(9Z))/PC(P-16:2(9Z,12E)/18:1(12Z)) --> PCO-16:0_18:1/PCP-16:2_18:1
+  p2 = paste0(.en(class_p), "[ -]*\\(", "([OP])-",.en(chain_multi_p), "\\)")
+  olipids = gsub(p2, "\\1\\2(\\3)", olipids)
+  olipids
+}
+
+
+.parse_molecule <- function(lipids_list) {
+  mol_p_en = paste0(.en(class_p), "[ -]*\\(", .en(chain_multi_p), "\\)",.en(notes_multi), "*")
+  matches = str_match(lipids_list, mol_p_en)
+  colnames(matches) = c("match", "class", "chains", "notes")
+  data.frame(input=lipids_list, matches)
+}
+
+
 .clean_molecule_name <- function(lipids_list) {
   olipids = trimws(lipids_list)
   
   # PC(O-32:0) --> PCO-32:0
-  p2 = paste0(class_p, "[ -]*\\(", "([OP]-\\d{1,2}:\\d{1,2}[^)]*)", "\\)")
+  # PC(O-16:0_18:1)/PC(P-16:2_18:1) --> PCO-16:0_18:1/PCP-16:2_18:1
+  # PC(O-16:0/18:2(9Z))/PC(P-16:2(9Z,12E)/18:1(12Z)) --> PCO-16:0_18:1/PCP-16:2_18:1
+  p2 = paste0(.en(class_p), "[ -]*\\(", .en("[OP]-", chain_multi_p), "\\)")
   olipids = gsub(p2, "\\1\\2", olipids)
   
   # Cer(d18:0/C18:0) --> Cer d18:0/18:0
-  p2 = paste0(class_p, "[ -]*\\(", "([[:alpha:]]{0,1}\\d{1,2}:\\d{1,2}[^)]*)", "\\)")
+  p2 = paste0(.en(class_p), "[ -]*\\(", chain_multi_p, "\\)")
   olipids = gsub(p2, "\\1 \\2", olipids)
   
   # Cer d18:0/C18:0 --> Cer 18:0/18:0
@@ -76,7 +105,8 @@ itsd = paste0("^", mol_p, ".*", isotope_p)#"^([[:alnum:]]{2,7}) (\\d{2}:\\d{1,2}
   olipids = sub("Lyso\\s?P", "LP", olipids)
   
   # 18:1(d7) LPC --> LPC 18:1(d7)
-  olipids = sub("(^[[:digit:]+][^[:blank:]]*)[[:blank:]?](.*$)", "\\2 \\1",  olipids)
+  p2 = paste0("^", chain_p, "(\\s*.*)\\W", class_p, "(.*$)")
+  olipids = sub(p2, "\\3 \\1\\2\\4",  olipids)
   
   # LPC 18:1-d7 --> LPC 18:1(d7)
   olipids = sub("[^(](d\\d)(\\D|$)", "(\\1)\\2",  olipids)
