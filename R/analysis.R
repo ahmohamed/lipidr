2f19082ff1bad2586945ca803c46fdc160fb9bd4
Ahmed Mohamed <mohamed@kuicr.kyoto-u.ac.jp>
2019-03-25 14:45:31 +1000

    minor typo in de_design exmaples

--- R/analysis.R
+++ R/analysis.R
@@ -57,7 +57,7 @@ de_analysis = function(..., data, measure="Area", group_col=NULL){
 #' # Using formula
 #' de_results = de_design(coef="groupHighFat_water", design = ~ group, data=data_normalized, measure="Area")
 #' 
-# Using design matrix
+#' # Using design matrix
 #' model.matrix(~ group, data=colData(data_normalized))
 #' de_results = de_design(coef="groupHighFat_water", design = design, data=data_normalized, measure="Area")
 de_design <- function(..., coef=NULL, design, data, measure="Area") {
