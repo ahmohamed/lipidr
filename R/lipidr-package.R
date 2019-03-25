4215004dc6cc271c1e549e61986778cb07288c2d
Ahmed Mohamed <mohamed@kuicr.kyoto-u.ac.jp>
2019-03-25 10:48:10 +1000

    Dataset containing normalized dataset and function to generate it

--- R/lipidr-package.R
+++ R/lipidr-package.R
@@ -39,3 +39,16 @@ NULL
 .onAttach <- function(lib, pkg) {
    utils::data(lipidDefaults, envir=.myDataEnv)
 }
+
+# Internal function to update the dataset, in case of future changes.
+# Should be called once before each package release.
+.updateData <- function(pkgpath) {
+  datadir = system.file("extdata", package="lipidr")
+  filelist = list.files(datadir, "data.csv", full.names = TRUE)
+  d = read_skyline(filelist)
+  clinical_file = system.file("extdata", "clin.csv", package="lipidr")
+  d = add_sample_annotation(d, clinical_file)
+  d_summarized = summarize_transitions(d, method = "average")
+  data_normalized = normalize_pqn(d_summarized, measure = "Area", exclude = "blank", log = TRUE)
+  save(data_normalized, file=file.path(pkgpath, "data/data_normalized.rda"))
+}
