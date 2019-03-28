97516df56b6f1320229e99b08a0c3262f7d5d1ad
Ahmed Mohamed <mohamed@kuicr.kyoto-u.ac.jp>
2019-03-28 17:06:02 +1000

    roxygenize

--- R/utils-misc.R
+++ R/utils-misc.R
@@ -30,9 +30,6 @@
 #' @param collapse Collapse all strings to create a single pattern (using `|`).
 #'
 #' @return regex-escaped string to be used for pattern matching
-#' @importFrom
-#'
-#' @examples
 .as_regex <- function(strings, collapse = FALSE, prefix="", suffix="") {
   ret <- gsub("([.|()\\^{}+$*?]|\\[|\\])", "\\\\\\1", strings)
   if (collapse) {
