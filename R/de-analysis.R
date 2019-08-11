3267e35d7c4f2b9eaa011b7e4976a599e583a25a
Ahmed Mohamed <mohamed@kuicr.kyoto-u.ac.jp>
2019-08-11 16:03:10 +1000

    spelling mistakes

--- R/de-analysis.R
+++ R/de-analysis.R
@@ -198,4 +198,4 @@ plot_results_volcano <- function(de.results, show.labels = TRUE) {
 }
 
 # colnames used in topTable
-utils::globalVariables(c("logFC", "P.Value", "adj.P.Val", "contrast"))
+utils::globalVariables(c("logFC", "AveExpr", "P.Value", "adj.P.Val", "contrast"))
