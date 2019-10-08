a818f78d22666dc33bcd163d2a456381cab85548
Ahmed Mohamed <mohamed@kuicr.kyoto-u.ac.jp>
2019-10-08 13:57:35 +1000

    skin_on_bioc for mw tests

--- tests/testthat/test-mw.R
+++ tests/testthat/test-mw.R
@@ -1,17 +1,7 @@
 library(lipidr)
-d <- read_skyline(c("A1.csv", "F2.csv"))
-
-expect_assay_equal <- function(e1, e2, measure) {
-  mat <- assay(e1, measure)
-  rownames(mat) <- rowData(e1)$Molecule
-
-  sum_mat <- assay(e2, measure)
-  rownames(sum_mat) <- rowData(e2)$Molecule
-  expect_equal(mat, sum_mat[rownames(mat), ] )
-}
-
 context("test-mw")
 test_that("can list studies for a specific keyword", {
+  skip_on_bioc()
   skip_if_offline("www.metabolomicsworkbench.org")
   res <- list_mw_studies(keyword = "lipidomics")
   expect_gt(nrow(res), 30)
@@ -20,6 +10,7 @@ test_that("can list studies for a specific keyword", {
   expect_false(any(na_cols))
 })
 test_that("can fetch study by id", {
+  skip_on_bioc()
   skip_if_offline("www.metabolomicsworkbench.org")
   expect_warning(
     res <- fetch_mw_study("ST001111"),
