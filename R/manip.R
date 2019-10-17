e75f134c16247247b318a9815a6c4bb3f4da7c18
sperritt <56667681+sperritt@users.noreply.github.com>
2019-10-17 16:04:57 +1100

    Added filter by blanks function

--- R/manip.R
+++ R/manip.R
@@ -141,4 +141,30 @@ filter_by_cv <- function(data, cv.cutoff = 20, measure = "Area") {
   data[keep_molecules, ]
 }
 
+
+#' Used to filter out molecules that read below a density threshold.
+#'
+#' @param data LipidomicsExperiment object. 
+#' @param fold.cutoff Fold Chain threshold (numeric). Default is '10'.
+#' @param measure Which is used to measure fold chain, usually log2(Area) (default).
+#'
+#' @return LipidomicsExperiment object with molecules filtered
+#' @export
+#'
+#' @examples
+#' data(data_normalized)
+#' filter_by_blanks(data_normalized)
+filter_by_blanks <- function(data, fold.cutoff = 10, measure = "Area") {
+  blanks <- lipidr:::.is_blank(data)
+  blanks.molecule <- assay(data[,blanks], measure)
+  blanks.molecule[is.na(blanks.molecule)] <- 0
+  blanks.mean <- rowMeans(blanks.molecule)
+  
+  nblanks.molecule <- assay(data[,!blanks], measure)
+  nblanks.mean <- rowMeans(nblanks.molecule, na.rm=T)
+  
+  keep.molecules <- nblanks.mean > blanks.mean*fold.cutoff
+  data[keep.molecules,]
+  
+}
 utils::globalVariables(c("new_names"))
